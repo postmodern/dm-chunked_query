298314e4cb814f288d9ecd18c5099ca8e820ae31
Postmodern <postmodern.mod3@gmail.com>
2011-03-03 18:58:34 -0800

    Add the each_chunk convenience method.

--- lib/dm-chunked_query/mixin.rb
+++ lib/dm-chunked_query/mixin.rb
@@ -15,6 +15,27 @@ module DataMapper
       def chunks(per_chunk)
         Chunks.new(self,per_chunk)
       end
+
+      #
+      # Enumerate over every chunk.
+      #
+      # @param [Integer] per_chunk
+      #   The number of resources per-chunk.
+      #
+      # @yeild [chunk]
+      #   A chunk of resources within the query.
+      #
+      # @yieldparam [DataMapper::Collection] chunk
+      #   A collection of resources that makes up the chunk.
+      #
+      # @return [Chunks]
+      #   The abstract collection of chunks from the query.
+      #
+      # @since 0.2.0
+      #
+      def each_chunk(per_chunk,&block)
+        chunks.each(&block)
+      end
     end
   end
 end
