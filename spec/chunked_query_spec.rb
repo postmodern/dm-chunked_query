041b00ab62fc4e9e47a05d2d0536c13be0ad7f7b
Postmodern <postmodern.mod3@gmail.com>
2011-05-16 02:17:59 -0700

    Added specs for each_chunk and batch.

--- spec/chunked_query_spec.rb
+++ spec/chunked_query_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
 describe DataMapper::ChunkedQuery do
+  subject { TestModel }
+
   it "should allow chunked queries directly from Models" do
-    TestModel.chunks(1).length.should == 100
+    subject.chunks(1).length.should == 100
   end
 
   it "should allow chunked queries from other queries" do
-    TestModel.all(:number.lte => 50).chunks(1).length.should == 50
+    subject.all(:number.lte => 50).chunks(1).length.should == 50
+  end
+
+  it "should allow iterating over every resource" do
+    count = 0
+
+    subject.each_chunk(2) { |chunk| count += chunk.length }
+
+    count.should == 100
+  end
+
+  it "should allow processing records in batch" do
+    count = 0
+
+    subject.batch(2) { |resource| count += 1 }
+
+    count.should == 100
   end
 end
